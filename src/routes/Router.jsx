8a449c71354d3189681b15e5e6ab2ea1b08d12f4
Shefaul-22 <cse1805022brur@gmail.com>
2026-01-17 20:10:11 +0600

    Register, Login page related basic structure  added

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import HomePage from "../Pages/Home/HomePage";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../Pages/AuthPages/Login";
+import Register from "../Pages/AuthPages/Register";
 
 
 export const router = createBrowserRouter([
-    
+
     {
         path: "/",
         Component: RootLayout,
@@ -14,5 +17,24 @@ export const router = createBrowserRouter([
                 Component: HomePage
             }
         ]
+    },
+
+    // AuthLayout related
+    {
+        path: "/",
+        Component: AuthLayout,
+        children: [
+
+            {
+                path: "register",
+                Component: Register
+            },
+
+            {
+                path: "login",
+                Component: Login
+            }
+        ]
+
     }
 ])
