b4960fb4f607c59c26cad1614f6fb90941aa8c73
Shefaul-22 <cse1805022brur@gmail.com>
2026-01-17 19:54:16 +0600

    UseAuth,UseAxios custom hooks added

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
-import HomePage from "../components/Home/HomePage";
+import HomePage from "../Pages/Home/HomePage";
+
 
 export const router = createBrowserRouter([
     
